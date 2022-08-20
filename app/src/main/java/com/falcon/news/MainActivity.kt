d1523966248eb2a55741335f65260ddd4c924c24
Jie Wang <jie@JiedeMacBook-Air.local>
2022-08-20 09:14:03 +0800

    Init Project

---
+++ app/src/main/java/com/falcon/news/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.falcon.news
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.falcon.news.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
